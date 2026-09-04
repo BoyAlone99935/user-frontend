4bfd6a953823a446ef09a0b8cb8ee55565e1b247
BoyAlone99935 <kingsleychijioke266@gmail.com>
2026-09-03 23:01:15 -0700

    add purchase ticket page

--- src/components/Purchase.jsx
+++ src/components/Purchase.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import TicketStub from "../../user-frontend-main/src/components/TicketStub";
 import Navbar from "../../user-frontend-main/src/components/Navbar";
